169262bce1670b4a82bcf7713d3b401e3f47c6f6
Ionică Bizău <bizauionica@yahoo.com>
2013-09-03 09:21:33 +0300

    set values, min, max

--- input-number.js
+++ input-number.js
@@ -6,8 +6,13 @@ $("input[type='number']").each(function () {
     $this.replaceWith($clone);
     
     var $btn = $("<button>").addClass("btn btn-mini");
-    var $btnPlus = $btn.clone().append("<i class='icon icon-plus'></i>");
-    var $btnMinus = $btn.clone().append("<i class='icon icon-minus'></i>");
+    
+    var $btnPlus = $btn.clone().append("<i class='icon icon-plus'></i>").on("click", function () {
+        increment($(this));
+    });
+    var $btnMinus = $btn.clone().append("<i class='icon icon-minus'></i>").on("click", function () {
+        decrement($(this));
+    });
     
     var $btnGroup = $("<div>")
                         .addClass("btn-group btn-group-vertical number-btn")
@@ -16,3 +21,66 @@ $("input[type='number']").each(function () {
     
     $clone.after($btnGroup);
 });
+
+function getInput (button) {
+    return $input = button.parent().prev();
+}
+
+function increment (button) {
+    var $input = getInput(button);
+    var value = getValue($input);
+    ++value;
+    setValue($input, value);
+}
+
+function decrement (button) {
+    var $input = getInput(button);
+    var value = getValue($input);
+    --value;
+    setValue($input, value);
+}
+
+function setValue (input, value) {
+    var $input = input;
+    var limits = getLimits($input);
+    
+    // both limits
+    if (limits.max && limits.min) {
+        if (value <= limits.max && value >= limits.min) {
+            $input.val(value);
+        }
+        return;
+    }
+    
+    // only min
+    if (limits.min) {
+        if (value >= limits.min) {
+            $input.val(value);
+        }
+        return;
+    }
+    
+    // only max
+    if (limits.max) {
+        if (value <= limits.max) {
+            $input.val(value);
+        }
+        return;
+    }
+    
+    // no limits
+    $input.val(value);
+}
+
+function getValue (input) {
+    var $input = input;
+    return $($input).val() || 0;
+}
+
+function getLimits(input) {
+    var $input = input;
+    return {
+        min: $($input).attr("min"),
+        max: $($input).attr("max")
+    };
+}
